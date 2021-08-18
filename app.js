629af103648f37d3038aa407c809be2789d05515
lama25 <jaberlama25@gmail.com>
2021-08-18 15:25:55 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,58 +1,62 @@
-var AWS = require('aws-sdk');
-// Set the region 
-AWS.config.update({region: 'us-east-2'});
+var port = process.env.PORT || 3000,
+    http = require('http'),
+    fs = require('fs'),
+    AWS = require('aws-sdk');
 
-// Create the DynamoDB service object
-var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 
-var params = {
-  TableName: 'ProductCatalog',
-  Item: {
-    'Id' : {N: '005'}
-    //'CUSTOMER_NAME' : {S: 'Richard Roe'}
-  }
-};
-
-// // Call DynamoDB to add the item to the table
-// ddb.putItem(params, function(err, data) {
-//   if (err) {
-//     console.log("Error", err);
-//   } else {
-//     console.log("Success", data);
-//   }
-// });
-
-const params2 = {
-  // Specify which items in the results are returned.
-  FilterExpression: "Id > :s AND Id < :e",
-  // Define the expression attribute value, which are substitutes for the values you want to compare.
-  ExpressionAttributeValues: {
-    ":s": {N: '0'},
-    ":e": {N: '100'}
-  },
-  // Set the projection expression, which are the attributes that you want.
-  ProjectionExpression: "Id",
-  TableName: "ProductCatalog",
-};
-
-ddb.scan(params2, function (err, data) {
-  if (err) {
-    console.log("Error", err);
-    response.end("error\n");
-
-  } else {
-    console.log("Success", data);
-    var ids = "";
-    data.Items.forEach(function (element, index, array) {
-      ids += " " + element.Id['N'];
-      console.log(
-          "printing",
-          element.Id
-      );
-
-      console.log("asdasd " + ids);
+var app = http.createServer(function (req, res) {
+  if (req.url.indexOf('/img') != -1) {
+    var filePath = req.url.split('/img')[1];
+    fs.readFile(__dirname + '/public/img' + filePath, function (err, data) {
+      if (err) {
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.write('Error 404: Resource not found.');
+        console.log(err);
+      } else {
+        res.writeHead(200, {'Content-Type': 'image/svg+xml'});
+        res.write(data);
+      }  
+      res.end();
     });
-    response.end("Hello World\n" + ids + "\n");
-
+  } else if (req.url.indexOf('/js') != -1) {
+    var filePath = req.url.split('/js')[1];
+    fs.readFile(__dirname + '/public/js' + filePath, function (err, data) {
+      if (err) {
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.write('Error 404: Resource not found.');
+        console.log(err);
+      } else {
+        res.writeHead(200, {'Content-Type': 'text/javascript'});
+        res.write(data);
+      }  
+      res.end();
+    });
+  } else if(req.url.indexOf('/css') != -1) {
+    var filePath = req.url.split('/css')[1];
+    fs.readFile(__dirname + '/public/css' + filePath, function (err, data) {
+      if (err) {
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.write('Error 404: Resource not found.');
+        console.log(err);
+      } else {
+        res.writeHead(200, {'Content-Type': 'text/css'});
+        res.write(data);
+      }
+      res.end();
+    });
+  } else {
+    fs.readFile(__dirname + '/public/index.html', function (err, data) {
+      if (err) {
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.write('Error 404: Resource not found.');
+        console.log(err);
+      } else {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+      }
+      res.end();
+    });
   }
-});
+}).listen(port, '0.0.0.0');
+
+module.exports = app;
