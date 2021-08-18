45b117f85b27c75e207b8506a22f7aa4890e528d
lama25 <jaberlama25@gmail.com>
2021-08-18 15:16:38 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,97 +1,58 @@
-var port = process.env.PORT || 3000,
-    http = require('http'),
-    fs = require('fs'),
-    AWS = require('aws-sdk');
-
-var app = http.createServer(function (req, res) {
-  AWS.config.update({region: 'us-east-2'});
+var AWS = require('aws-sdk');
+// Set the region 
+AWS.config.update({region: 'us-east-2'});
 
 // Create the DynamoDB service object
-  var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
-  if (req.url.indexOf('/img') != -1) {
-    var filePath = req.url.split('/img')[1];
-    fs.readFile(__dirname + '/public/img' + filePath, function (err, data) {
-      if (err) {
-        res.writeHead(404, {'Content-Type': 'text/plain'});
-        res.write('Error 404: Resource not found.');
-        console.log(err);
-      } else {
-        res.writeHead(200, {'Content-Type': 'image/svg+xml'});
-        res.write(data);
-      }  
-      res.end();
-    });
-  } else if (req.url.indexOf('/js') != -1) {
-    var filePath = req.url.split('/js')[1];
-    fs.readFile(__dirname + '/public/js' + filePath, function (err, data) {
-      if (err) {
-        res.writeHead(404, {'Content-Type': 'text/plain'});
-        res.write('Error 404: Resource not found.');
-        console.log(err);
-      } else {
-        res.writeHead(200, {'Content-Type': 'text/javascript'});
-        res.write(data);
-      }  
-      res.end();
-    });
-  } else if(req.url.indexOf('/css') != -1) {
-    var filePath = req.url.split('/css')[1];
-    fs.readFile(__dirname + '/public/css' + filePath, function (err, data) {
-      if (err) {
-        res.writeHead(404, {'Content-Type': 'text/plain'});
-        res.write('Error 404: Resource not found.');
-        console.log(err);
-      } else {
-        res.writeHead(200, {'Content-Type': 'text/css'});
-        res.write(data);
-      }
-      res.end();
-    });
-  } else {
-    fs.readFile(__dirname + '/public/index.html', function (err, data) {
-      if (err) {
-        res.writeHead(404, {'Content-Type': 'text/plain'});
-        res.end('Error 404: Resource not found.');
-      } else {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-      
-        res.end();
-        return;
-        // Set the region 
-     
-        const params2 = {
-          // Specify which items in the results are returned.
-          FilterExpression: "Id > :s AND Id < :e",
-          // Define the expression attribute value, which are substitutes for the values you want to compare.
-          ExpressionAttributeValues: {
-            ":s": {N: '0'},
-            ":e": {N: '100'}
-          },
-          // Set the projection expression, which are the attributes that you want.
-          ProjectionExpression: "Id",
-          TableName: "ProductCatalog",
-        };
-        
-        ddb.scan(params2, function (err, data2) {
-          if (err) {
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.end("" + err);          
-          } else {
-            var ids = "";
-            data2.Items.forEach(function (element, index, array) {
-              ids += " " + element.Id['N'];
-            });
+var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 
-            
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.end(ids);
-          }
-        });
-        
-      }
-    });
+var params = {
+  TableName: 'ProductCatalog',
+  Item: {
+    'Id' : {N: '005'}
+    //'CUSTOMER_NAME' : {S: 'Richard Roe'}
   }
-}).listen(port, '0.0.0.0');
+};
 
-module.exports = app;
+// // Call DynamoDB to add the item to the table
+// ddb.putItem(params, function(err, data) {
+//   if (err) {
+//     console.log("Error", err);
+//   } else {
+//     console.log("Success", data);
+//   }
+// });
+
+const params2 = {
+  // Specify which items in the results are returned.
+  FilterExpression: "Id > :s AND Id < :e",
+  // Define the expression attribute value, which are substitutes for the values you want to compare.
+  ExpressionAttributeValues: {
+    ":s": {N: '0'},
+    ":e": {N: '100'}
+  },
+  // Set the projection expression, which are the attributes that you want.
+  ProjectionExpression: "Id",
+  TableName: "ProductCatalog",
+};
+
+ddb.scan(params2, function (err, data) {
+  if (err) {
+    console.log("Error", err);
+    response.end("error\n");
+
+  } else {
+    console.log("Success", data);
+    var ids = "";
+    data.Items.forEach(function (element, index, array) {
+      ids += " " + element.Id['N'];
+      console.log(
+          "printing",
+          element.Id
+      );
+
+      console.log("asdasd " + ids);
+    });
+    response.end("Hello World\n" + ids + "\n");
+
+  }
+});
