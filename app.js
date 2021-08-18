3eecd752847856212028ed454f5614d1807e57e0
lama25 <89072268+lama25@users.noreply.github.com>
2021-08-18 12:32:19 +0300

    sfsdf

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ var app = http.createServer(function (req, res) {
         ddb.scan(params2, function (err, data) {
           if (err) {
             res.writeHead(200, {'Content-Type': 'text/html'});
-            res.write(data);          
+            res.write(data + err);          
           } else {
             var ids = "";
             console.log("Success", data);
